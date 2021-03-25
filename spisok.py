c6bc79aea54dc1682026092d886250259f2df60a
zhanna <de-mambo@yandex.ru>
2021-03-25 19:13:46 +0300

    Скорректированное решение

--- spisok.py
+++ spisok.py
@@ -3,7 +3,7 @@ from random import randint
 numericList = [randint(-10, 10) for p in range(0, 20)]
 #можем заменить рандинт на i выведет по порядку, если число то его 20 раз
 
-#просто для себя вывожу максимальный элемент списка
+#вывожу максимальный элемент списка
 max(numericList)
 print("Максимальный элемент списка: ", max(numericList))
 
@@ -12,6 +12,7 @@ maxElementInList1 = 0
 for i in numericList:
     if i > maxElementInList1:
         maxElementInList1 = i
+maxIndex = numericList.index(maxElementInList1)
 
 #счет нулей и запись количества в счетчик
 zeroCounter = 0
@@ -21,20 +22,16 @@ for j in numericList:
 
 #добавление элементов в новый список по порядку, без нулей
 newNumericList = []
-for k in numericList:
-    if k != 0 and k < maxElementInList1:
-        newNumericList.append(k)
-        numericList.append(0)
-
-#поиск максимума в новом списке
-maxElementInList2 = 0
-for q in newNumericList:
-    if q > maxElementInList2:
-        maxElementInList2 = q
+for k in range(0, maxIndex + 1):
+    if numericList[k] != 0:
+        newNumericList.append(numericList[k])
+for m in range(zeroCounter):
+    newNumericList.append(0)
+for r in range(maxIndex + 1, len(numericList)):
+    if r != 0:
+        newNumericList.append(numericList[r])
 
 print("Cписок псевдослучайных чисел: ", numericList)
 print("Список чисел после решения задачи: ", newNumericList)
 print("Сколько нулей в списке: ", zeroCounter)
 print("Индекс максимального элемента списка А: ", numericList.index(maxElementInList1))
-print("Индекс максимального элемента списка B: ", newNumericList.index(maxElementInList2))
-
